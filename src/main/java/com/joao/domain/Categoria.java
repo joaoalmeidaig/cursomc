7134fe8af2d0c6681f7ba860f22f9d0e4cb59588
João Almeida <joaoalmeidapedrosa@gmail.com>
2022-02-18 21:09:11 -0300

    testando a primeira classe de dominio (controller) - categoria

---
+++ src/main/java/com/joao/domain/Categoria.java
@@ -0,0 +1,16 @@
+package com.joao.domain;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+}
